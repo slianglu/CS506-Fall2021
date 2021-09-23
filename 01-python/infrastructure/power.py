24d1b1e8879bee602ce2cae4aacae50df27f6fbc
slianglu <slianglu@bu.edu>
2021-09-23 09:46:04 -0400

    created power plant

--- 01-python/infrastructure/power.py
+++ 01-python/infrastructure/power.py
@@ -1,3 +1,15 @@
 def draw_power_plant():
-    print("power plant not found")
+    
+    print("           ) ) )                     ) ) )")
+    print("         ( ( (                      ( ( (")
+    print("       ) ) )                       ) ) )")
+    print("    (~~~~~~~~~)                 (~~~~~~~~~)")
+    print("     | POWER |                   | POWER |")
+    print("     |       |                   |       |")
+    print("     I      _._                  I       _._")
+    print("     I    /'   `\                I     /'   `\\")
+    print("     I   |   N   |               I    |   N   |")
+    print("     f   |   |~~~~~~~~~~~~~~|    f    |    |~~~~~~~~~~~~~~|")
+    print("   .'    |   ||~~~~~~~~|    |  .'     |    | |~~~~~~~~|   |")
+    print("  '______|___||__###___|____|/'_______|____|_|__###___|___|")
     return
